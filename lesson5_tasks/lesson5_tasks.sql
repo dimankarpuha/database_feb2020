1e839fea872f5df6c5194a1d1511004d9d6033fb
diman <dimankarpuha@gmail.com>
2020-03-09 00:40:40 +0300

    lesson5_tasks_new

--- lesson5_tasks/lesson5_tasks.sql
+++ lesson5_tasks/lesson5_tasks.sql
@@ -16,5 +16,15 @@ UPDATE users SET created_at = STR_TO_DATE(created_at ,'%d.%m.%Y %H:%i');
 ALTER TABLE users MODIFY COLUMN created_at DATETIME;
 
 
+#task 3
+# create table storehouses_products
+CREATE TABLE storehouses_products 
+	(id SERIAL,
+	value INT UNSIGNED);
+DESC storehouses_products;
+INSERT INTO storehouses_products (value)
+	VALUES (1),(344),(45),(65),(7678),(0),(234),(5),(0),(234),(0);
 
-	
+SELECT * FROM storehouses_products ORDER BY CASE WHEN value = 0 THEN TRUE END, value;
+
+
